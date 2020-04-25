995baddedaa286e3f7a5ca4f640ec2ebfd78e5c1
Zach Baugus <zbaugus@outlook.com>
2020-04-25 11:07:41 -0400

    Changed textbox to read only

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -57,6 +57,7 @@
             this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.resultBox.Location = new System.Drawing.Point(12, 12);
             this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
             this.resultBox.Size = new System.Drawing.Size(346, 80);
             this.resultBox.TabIndex = 0;
             this.resultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
